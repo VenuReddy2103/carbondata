5ac3bfb5dcaca9dda8c784f89116f1df9775f1be
litao <litao_xidian@126.com>
2019-11-12 07:43:40 +0800

    generate the hash code ID

--- core/src/main/java/org/apache/carbondata/core/util/CustomIndex.java
+++ core/src/main/java/org/apache/carbondata/core/util/CustomIndex.java
@@ -22,7 +22,9 @@ import java.util.Map;
 public interface CustomIndex<SourceElementType, Query, Result> {
   void validateOption(Map<String, String> properties) throws Exception;
 
-  String generate(List<SourceElementType> columns) throws Exception;
+  Long generate(List<SourceElementType> columns) throws Exception;
 
   Result query(Query query) throws Exception;
+
+  void init(Map<String, String> properties) throws Exception;
 }
